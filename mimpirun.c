433f91a2c5e6a133debac49754ac2e9cdac4998d
macjakku <macjakku@gmail.com>
2023-12-27 01:08:57 +0100

    Added opening channels and passing them to processes

--- mimpirun.c
+++ mimpirun.c
@@ -16,35 +16,101 @@ int main(int argc, char* argv[]) {
 
     ASSERT_SYS_OK(setenv("MIMPI_world_size", argv[1], 1));
 
-    int** comm_pipes = malloc(world_size*sizeof(int));
+    channels_init();
+
+    int** read_pipes = malloc(world_size*sizeof(int*));
+    int** write_pipes = malloc(world_size*sizeof(int*));
     for(int i=0; i<world_size; i++)
     {
-        comm_pipes[i] = malloc(world_size*sizeof(int));
+        read_pipes[i] = malloc(world_size*sizeof(int));
+        write_pipes[i] = malloc(world_size*sizeof(int));
     }
 
     for(int i=0; i<world_size; i++)
     {
-        for(int j=i+1; j<world_size; j++)
+        for(int j=0; j<world_size; j++)
         {
-            int fds[2];
-            channel(fds);
-            comm_pipes[]
+            if(i==j)
+            {
+                read_pipes[i][i]=0;
+                write_pipes[i][i]=0;
+            }
+            else
+            {
+                int fds[2];
+                ASSERT_SYS_OK(channel(fds));
+                read_pipes[i][j]=fds[0];    //read and write ends of a pipe from the ith process to the jth
+                write_pipes[i][j]=fds[1];   //process are saved here
+            }
+            
         }
     }
-
-    char* world_rank = malloc(2*sizeof(char));
+    char* name1 = malloc(32*sizeof(char));
+    char* name2 = malloc(32*sizeof(char));
+    char* value1 = malloc(10*sizeof(char));
+    char* value2 = malloc(10*sizeof(char));
+    char* world_rank = malloc(10*sizeof(char));
     for (int i=0; i<world_size; i++)
     {
         sprintf(world_rank, "%d", i);
         ASSERT_SYS_OK(setenv("MIMPI_world_rank", world_rank, 1));
+
+        for(int j=0; j<world_size; j++)
+        {
+            if(j!=i)
+            {
+                sprintf(name1, "MIMPI_channel_from_%d",j);
+                sprintf(value1, "%d", write_pipes[j][i]);
+                sprintf(name2, "MIMPI_channel_to_%d",j);
+                sprintf(value2, "%d", read_pipes[i],[j]);
+            }
+        }
+
         pid_t pid;
         ASSERT_SYS_OK(pid = fork());
         if(!pid)
         {
+            for(int j=0; j<world_size; j++)
+            {
+                for(int k=0; k<world_size; k++)
+                {
+                    if(j!=k && j!=i)
+                    {
+                        close(read_pipes[i][j]);
+                        close(write_pipes[j][i]);
+                    }
+                }
+            }
             execvp(prog_name, prog_args);
         }
     }
+    free(name1);
+    free(name2);
+    free(value1);
+    free(value2);
     free(world_rank);
+
+    for(int i=0; i<world_size; i++)
+    {
+        for(int j=0; j<world_size; j++)
+        {
+            if(i!=j)
+            {
+                close(read_pipes[i][j]);
+                close(write_pipes[i][j]);
+            }
+        }
+    }
+
+    for(int i=0; i<world_size; i++)
+    {
+        free(read_pipes[i]);
+        free(write_pipes[i]);
+    }
+    free(read_pipes);
+    free(write_pipes);
+
+    channels_finalize();
 
     for(int i=0; i<world_size; i++)
     {
