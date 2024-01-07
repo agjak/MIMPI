b40c59f58eb157ad3840f98dee78c824135c9600
macjakku <macjakku@gmail.com>
2024-01-07 13:29:04 +0100

    zxc

--- mimpi.c
+++ mimpi.c
@@ -139,7 +139,7 @@ MIMPI_Retcode MIMPI_sync_reduce_recv(
     {
         printf("B\n");
     }
-    if(chrecv(recv_fd, (void*)signal, 1)==0)
+    if(chrecv(recv_fd, (void*)signal, 1)<=0)
     {
         return MIMPI_ERROR_REMOTE_FINISHED;
     }
