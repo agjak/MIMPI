fa70a73978f563b882f37d67a7813e408c47ab73
macjakku <macjakku@gmail.com>
2024-01-07 02:09:06 +0100

    asdasd

--- mimpi.c
+++ mimpi.c
@@ -465,7 +465,7 @@ MIMPI_Retcode MIMPI_Recv(
             printf("0 %d\n", MIMPI_World_rank());
         }
         struct buffer_node *last_node=NULL;
-        struct buffer_node *node=message_buffers[source];
+        struct buffer_node *node=&message_buffers[source];
         while(node->message!=NULL)
         {
             if(MIMPI_World_rank()==8)
