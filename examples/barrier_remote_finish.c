da38572b2d5a62ec6088fa976587b0add84fc6bc
macjakku <macjakku@gmail.com>
2024-01-07 03:17:53 +0100

    asd

--- examples/barrier_remote_finish.c
+++ examples/barrier_remote_finish.c
@@ -15,6 +15,7 @@ int main(int argc, char **argv)
         // Should exit with MIMPI_ERROR_REMOTE_FINISHED
         // during or after MIMPI_Finalize call in process 0.
         MIMPI_Retcode ret = MIMPI_Barrier();
+        printf("%d %d\n",ret,world_rank);
         test_assert(ret == MIMPI_ERROR_REMOTE_FINISHED);
     }
 
