a8092bbeb0dce079094c9f559017cb871a204121
macjakku <macjakku@gmail.com>
2024-01-07 02:13:15 +0100

    sfsdf

--- mimpi.c
+++ mimpi.c
@@ -514,8 +514,7 @@ MIMPI_Retcode MIMPI_Recv(
                     {
                         printf("3c %d\n", MIMPI_World_rank());
                     }
-                    message_buffers[source]->message=node->next->message;
-                    message_buffers[source]->next=node->next->next;
+                    message_buffers[source]=node->next;
                 }
                 else
                 {
