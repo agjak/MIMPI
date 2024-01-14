c3f95303a7fee5e9cd1c58fc78c2751aba35b503
macjakku <macjakku@gmail.com>
2024-01-14 11:29:24 +0100

    fgf

--- examples/deadlock6.c
+++ examples/deadlock6.c
@@ -25,6 +25,7 @@ int main(int argc, char **argv)
         MIMPI_Send(data, sizeof(int) * 1000000, partner_rank, 2);
         MIMPI_Send(data, sizeof(int) * 1000000, partner_rank, 2);
     }
+    printf("waitin %d\n", world_rank);
     ASSERT_MIMPI_RETCODE(MIMPI_Recv(data, sizeof(int) * 1000000, partner_rank, 1), MIMPI_ERROR_DEADLOCK_DETECTED);
     printf("done %d\n", world_rank);
     MIMPI_Finalize();
